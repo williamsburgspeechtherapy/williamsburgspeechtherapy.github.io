a9e5f909c89d80a09f894501863ed73f2699500c
Ramin B <i8ramin@gmail.com>
2015-05-30 15:22:56 -0400

    copy gitignore over

--- gulpfile.js
+++ gulpfile.js
@@ -81,7 +81,7 @@ gulp.task("fonts", function () {
 
 // Copy xml and txt files to the "site" directory
 gulp.task("copy", function () {
-  return gulp.src(["serve/*.txt", "serve/*.xml"])
+  return gulp.src(["serve/*.txt", "serve/*.xml", ".gitignore"])
     .pipe(gulp.dest("site"))
     .pipe($.size({ title: "xml & txt" }))
 });
